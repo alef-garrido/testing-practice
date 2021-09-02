969568668eadbc2741c8d04472fcd78878f095db
Alef Garrido <oa.garrido11@gmail.com>
2021-09-02 02:10:54 -0500

    Add extended functionality

--- stringLength.js
+++ stringLength.js
@@ -1,5 +1,21 @@
-function stringLength(str) {
-  return str.split("").length
-}
+ class String {
+   static stringLength = (str) => {
+     return this.stringChecker(str)
+   }
+   
+  static stringChecker = (str) => {
+    if(str.length < 1) {
 
-module.exports = stringLength
+      throw new Error('Empty string not allowed');
+
+    } else if (str.length > 10) {
+      
+      throw new Error('10 character limit exceeded');
+      
+    } 
+
+      return str.length
+
+  }
+}
+module.exports = String
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,10 +1,21 @@
-const stringLength = require('./stringLength')
+const String = require('./stringLength')
 
 test('Cup should return 3', () => {
   expect(
-    stringLength('cup'))
+    String.stringLength('cup'))
     .toBe(3)
 })
 
+test('Empty string should return error', () => {
+  expect( () =>
+    String.stringLength(''))
+    .toThrow(Error);
+})
+
+test('String over 10 characters should return error', () => {
+  expect( () =>
+    String.stringLength('Honorificabilitudinitatibus'))
+    .toThrow(Error);
+})
 
 
